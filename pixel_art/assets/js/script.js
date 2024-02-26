99b953a407854cb1875fc06bc0e4ff638f9d834b
Paulo Henrique Alves de Almeida <158237204+paulo-henrique-almeida@users.noreply.github.com>
2024-02-26 02:56:24 -0300

    correção de bug

--- pixel_art/assets/js/script.js
+++ pixel_art/assets/js/script.js
@@ -1,31 +1,31 @@
 // adquirindo elementos
 let container = document.querySelector(".container");
 let btnCriarGrade = document.getElementById("btn-criar-grade");
-let btnExcluirGrade = document.getElementById("btn-excluir-grade");
+let btnLimparGrade = document.getElementById("btn-limpar-grade");
 let larguraGrade = document.getElementById("largura");
 let alturaGrade = document.getElementById("altura");
 let inputCor = document.getElementById("input-cor");
-let btnLimpar = document.getElementById("btn-limpar");
-let btnPintar = document.getElementById("paint-btn");
+let btnApagar = document.getElementById("btn-apagar");
+let btnPintar = document.getElementById("btn-pintar");
 let valorLargura = document.getElementById("valor-largura");
 let valorAltura = document.getElementById("valor-altura");
 
 let eventos = {
     mouse: {
-        baixo: "mouseParaBaixo",
-        mover: "mouseMoveu",
-        cima: "mouseParaCima"
+        baixo: "mousedown",
+        mover: "mousemove",
+        cima: "mouseup"
     },
     toque: {
-        baixo: "comecoToque",
-        mover: "toqueMoveu",
-        cima: "fimToque"
-    },
+        baixo: "touchstart",
+        mover: "touchmove",
+        cima: "touchend"
+    }
 };
 
 let tipoDispositivo = "";
-let desenhar = false;
-let limpar = false;
+let pintar = false;
+let apagar = false;
 
 const ehDispositivoToque = () => {
     try {
@@ -44,24 +44,79 @@ btnCriarGrade.addEventListener('click', () => {
     container.innerHTML = '';
 
     let contador = 0;
-    for (let i = 0; i < alturaGrade.ariaValueMax; i++) {
-        count += 2;
-        let div = document.createEvent('div');
+    for (let i = 0; i < alturaGrade.value; i++) {
+        contador += 2;
+        let div = document.createElement('div');
         div.classList.add('linha-grade');
 
-        for (let j = 0; j < larguraGrade.ariaValueMax; j++) {
-            count += 2;
-            let col = document.createEvent('div');
+        for (let j = 0; j < larguraGrade.value; j++) {
+            contador += 2;
+            let col = document.createElement('div');
             col.classList.add('coluna-grade');
             col.setAttribute('id', `coluna=grade-${contador}`);
+
             col.addEventListener(eventos[tipoDispositivo].baixo, () => {
-                desenhar = true;
-                if (limpar) {
+                pintar = true;
+                if (apagar) {
                     col.style.backgroundColor = "transparente";
                 } else {
                     col.style.backgroundColor = inputCor.value;
                 }
             });
+
+            col.addEventListener(eventos[tipoDispositivo].mover, (e) => {
+                let idElemento = document.elementFromPoint(
+                    !ehDispositivoToque() ? e.clientX : e.touches[0].clientX,
+                    !ehDispositivoToque() ? e.clientY : e.touches[0].clientY,
+                ).id;
+                checker(idElemento);
+            });
+
+            col.addEventListener(eventos[tipoDispositivo].cima, () => {
+                pintar = false;
+            });
+
+            div.appendChild(col);
         }
+
+        container.appendChild(div);
     }
 });
+
+function checker(idElemento) {
+    let colunasGrade = document.querySelectorAll('.coluna-grade');
+    colunasGrade.forEach((elemento) => {
+        if (idElemento == elemento.id) {
+            if (pintar && !apagar) {
+                elemento.style.backgroundColor = inputCor.value;
+            } else if (pintar && apagar) {
+                elemento.style.backgroundColor = "transparent";
+            }
+        }
+    });
+}
+
+btnLimparGrade.addEventListener('click', () => {
+    container.innerHTML = '';
+});
+
+btnApagar.addEventListener('click', () => {
+    apagar = true;
+});
+
+btnPintar.addEventListener('click', () => {
+    apagar = false;
+});
+
+larguraGrade.addEventListener('input', () => {
+    valorLargura.innerHTML = larguraGrade.value.toString().padStart(2, '0');
+});
+
+alturaGrade.addEventListener('input', () => {
+    valorAltura.innerHTML = alturaGrade.value.toString().padStart(2, '0');
+});
+
+window.onload = () => {
+    alturaGrade.value = 0;
+    larguraGrade.value = 0
+};
